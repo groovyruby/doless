dfd0ed38082e5739b76b2501a557e579173b25ae
Maciej Litwiniuk <maciej@galdomedia.pl>
2010-10-24 11:34:37 +0200

    root in routes

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ Doless::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "dashboards#index"
 
   # See how all your routes lay out with "rake routes"
 
