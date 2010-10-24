799f520c29dd06f32961249ff126b42b4ee4e545
Maciej Litwiniuk <maciej@galdomedia.pl>
2010-10-24 10:47:37 +0200

    some small changes to user model

--- config/application.rb
+++ config/application.rb
@@ -30,14 +30,18 @@ module Doless
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :en
 
-        config.action_view.javascript_expansions[:defaults] = %w()
+    config.action_view.javascript_expansions[:defaults] = %w()
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
-
+    config.generators do |g|
+      g.test_framework  :rspec
+      #g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
+        
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
   end
--- db/migrate/20101023194426_devise_create_users.rb
+++ db/migrate/20101023194426_devise_create_users.rb
@@ -5,19 +5,20 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.recoverable
       t.rememberable
       t.trackable
-
-      # t.confirmable
-      # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
-      # t.token_authenticatable
-
+      t.confirmable
+      t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
+      t.token_authenticatable
+      
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps
     end
 
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
-    # add_index :users, :confirmation_token,   :unique => true
-    # add_index :users, :unlock_token,         :unique => true
+    add_index :users, :confirmation_token,   :unique => true
+    add_index :users, :unlock_token,         :unique => true
   end
 
   def self.down
