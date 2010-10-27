c9fda487d7eba2ad2e8a8cd5667799cc3db1360f
Maciej Litwiniuk <maciej@galdomedia.pl>
2010-10-27 10:50:44 +0200

    assets shown in ticket sidebar

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ActiveRecord::Base
   has_friendly_id :name, :use_slug => true
   
+  has_many :assets
   has_many :permissions
   has_many :users, :through=>:permissions
   #has_many :managers, :class_name => "user", :foreign_key => "user_id", :throught=>:permissions, :where=>['permissions.permission_type>=?', Permission::PERMISSION_TYPES[:manager]
