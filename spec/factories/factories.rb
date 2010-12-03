a15c3a3308cf874f94117db563311093250dba84
Maciej Litwiniuk <maciej@galdomedia.pl>
2010-12-03 18:20:28 +0100

    some more factories

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,11 +1,29 @@
 Factory.sequence :email do |n|
   "person#{n}@example.com"
 end
-    
+
+Factory.sequence :name do |n|
+  "sample_name_#{n}"
+end
+
 Factory.define :user do |f|
   f.first_name "Maciej"
   f.last_name "Litwiniuk"
   f.password "abcdefg1"
   f.password_confirmation "abcdefg1"
   f.email { Factory.next(:email) }
+end
+
+
+Factory.define :project do |f|
+  f.name { Factory.next(:name) }
+end
+
+
+Factory.define :task do |f|
+  f.title { Factory.next(:name) }
+end
+
+Factory.define :task_with_project, :parent=>:task do |f|
+  f.association :project, :factory=>:project
 end
